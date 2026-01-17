6364e293ec8025164a13b94c60d45d689db73f04
mahmutozerg <mahmutozerg@gmail.com>
2026-01-17 22:26:55 +0300

    .

--- scrapper-pw/constants/errorConstants.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-const GeneralFailure = "Scrapper failed "
-const FailureNewPage = "Failed To Create New Page "
-const InvalidUrl = "Missing scheme or host "
-const FailedGoTo = "Failed to Go to page "
-const FailedLaunchLocalBrowser = "Failed to open local browser"
